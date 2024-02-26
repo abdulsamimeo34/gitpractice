c12e0517f8743972fc8782639504efcff24a177f
abdulsamimeo34 <160492688+abdulsamimeo34@users.noreply.github.com>
2024-02-26 19:19:42 +0500

    Add files via upload

---
+++ var.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var first = 'Abdul Sami';
+console.log(first);
---
+++ var.ts
@@ -0,0 +1,5 @@
+import { Console } from "console"
+
+ var first = 'Abdul Sami'
+ console.log(first);
+ 
